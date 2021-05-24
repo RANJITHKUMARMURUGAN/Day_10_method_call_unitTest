f4796f30bd77c6303265b2dc33b9d38d33b5252c
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-24 16:48:18 +0530

    [Ranjith] added Simple.java

---
+++ Simple.java
@@ -0,0 +1,17 @@
+public class Simple {
+	 public int Addition(int a ,int b)
+		{
+			return a+b;
+		}
+	 public String conCat(String a,String b)
+	 {
+		return a+b; 
+	 }
+	public static void main(String[] args) {
+//	  int a=10;
+//	  int b=20;
+//	  Simple s = new Simple();
+//	  int temp=s.Addition(a, b); 
+//	  System.out.println(temp);
+	}
+}
